2ef9ac5666b63307fe714e2a83f0b81df73874da
jackplugged <sidneisimmon@gmail.com>
2018-07-10 18:03:55 -0300

    Menu problematico

--- src/components/sidebar/menu/multlevel.js
+++ src/components/sidebar/menu/multlevel.js
@@ -15,19 +15,7 @@ const multilevel = {
           href: "pages/tables/data.21.html",
           icon: "circle-o",
           text: "Level 2 item 1",
-          items: [
-            {
-              href: "pages/tables/data.31.html",
-              icon: "circle-o",
-              text: "Level 3 item 1"
-            },
-            {
-              href: "pages/tables/data.32.html",
-              icon: "circle-o",
-              text: "Level 3 item 2"
-            }
-          ]
-        }
+        }         
       ]
     }
   ]
--- src/components/sidebar/sidebar-menu/sidebar-item.jsx
+++ src/components/sidebar/sidebar-menu/sidebar-item.jsx
@@ -5,69 +5,23 @@ import TreeviewMenu from "../treeview/treeview-menu";
 import TreeviewItem from "../treeview/treeview-item";
 
 const recursiveTreeview = treeview => {  
-  if (!!treeview) { 
-    console.log(treeview); 
+  const { items, text, icon } = treeview;
+  if (!!items) {   
     return (     
-        treeview.map(({ text, icon, href, items }, key) => {        
+        items.map(({ text, icon, href, items }, key) => {        
           return (
-            <TreeviewItem key={key} treeview href={href} text={'Filho'} icon={icon}>                 
-            <TreeviewMenu>
-               <TreeviewItem href={href} text={text} icon={icon}/>
-            </TreeviewMenu>
-          </TreeviewItem>   
+            <Treeview text={text} icon={icon}>
+                <TreeviewMenu>
+                    <TreeviewItem href={href} text={text} icon={icon}/>
+                   
+                </TreeviewMenu>
+             </Treeview>
         );         
         })     
     );
   }
 };
 
-/*
-
-{/*     
-                  <TreeviewItem treeview href={href} text={'Filho'} icon={icon}>                 
-                    <TreeviewMenu>
-                       <TreeviewItem href={href} text={text} icon={icon}/>
-                    </TreeviewMenu>
-                  </TreeviewItem>  
-                  
-
-
-{recursiveTreeview(items)}    
-<Treeview key={key} text={treeview.text} icon={treeview.icon}>
-    <TreeviewMenu>
-      {treeview.map(({ item, items }, key) => (
-        <TreeviewItem key={key} {...item} />
-      ))}
-    </TreeviewMenu>
-</Treeview>
-*/
-
-const SidebarItem = ({ items, text, icon }) => {
-  return (
-    <Treeview text={text} icon={icon}>
-       <TreeviewMenu>
-       {items.map(({ text, icon, href, items }, key) => {        
-          return (
-            <Fragment key={key}>
-                <TreeviewItem href={href} text={text} icon={icon}/>
-                <TreeviewItem treeview text={'Filho'} icon={icon}>                 
-                    <TreeviewMenu>
-                       <TreeviewItem href={href} text={text} icon={icon}/>
-                      
-                       <TreeviewItem treeview href={href} text={'Filho'} icon={icon}>
-                          <TreeviewMenu>
-                            <TreeviewItem href={href} text={text} icon={icon}/>   
-                          </TreeviewMenu>
-                       </TreeviewItem>   
-                       
-                    </TreeviewMenu>
-                  </TreeviewItem> 
-            </Fragment>
-          );         
-        })}
-       </TreeviewMenu>
-    </Treeview>
-  );
-};
+const SidebarItem = ( props ) => recursiveTreeview(props);
 
 export default SidebarItem;
