daab768e261c1ea6a1cf6c5c6825f04b53f3031e
jackplugged <sidneisimmon@gmail.com>
2018-07-16 22:28:01 -0300

    ajustes

--- src/containers/menu/tables.jsx
+++ src/containers/menu/tables.jsx
@@ -1,21 +1,22 @@
 import { connect } from "react-redux";
-import SidebarItem from 'components/sidebar/sidebar-menu/sidebar-item';
+import SidebarItem from "components/sidebar/sidebar-menu/sidebar-item";
 
 const mapStateToProps = ({ notifications: { data } }) => {
-  return ({   
-    item:{
+  return {
+    item: {
       text: "Tables",
       icon: "table",
       notifications: [
         {
           text: "new",
-          color: 'blue'
-        },
+          color: "blue"
+        }
       ],
-      items: [{
+      items: [
+        {
           href: "/home",
           icon: "circle-o",
-          text: "Simple Tables",         
+          text: "Simple Tables"
         },
         {
           href: "pages/tables/data.html",
@@ -24,21 +25,21 @@ const mapStateToProps = ({ notifications: { data } }) => {
           notifications: [
             {
               text: data.notifications.length,
-              color: 'blue'
+              color: "blue"
             },
             {
               text: data.messages.length,
-              color: 'yellow'
+              color: "yellow"
             },
             {
               text: data.tasks.length,
-              color: 'red'
+              color: "red"
             }
-          ]         
+          ]
         }
       ]
     }
-  })  
+  };
 };
 
 export default connect(mapStateToProps)(SidebarItem);
--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeData, Tips } from "./Utils";
+import { makeData } from "./Utils";
 
 // Import React Table
 import ReactTable from "react-table";
@@ -16,67 +16,66 @@ class Home extends React.Component {
     const { data } = this.state;
     return (
       <div>
-        <br />
-        <strong>Open your console and hover over some cells!</strong>
-        <br />
-        <br />
-        <ReactTable
-          data={data}
-          columns={[
-            {
-              Header: "Name",
-              columns: [
-                {
-                  Header: "First Name",
-                  accessor: "firstName"
-                },
-                {
-                  Header: "Last Name",
-                  id: "lastName",
-                  accessor: d => d.lastName
-                }
-              ]
-            },
-            {
-              Header: "Info",
-              columns: [
-                {
-                  Header: "Age",
-                  accessor: "age"
-                },
-                {
-                  Header: "Status",
-                  accessor: "status"
-                }
-              ]
-            },
-            {
-              Header: "Stats",
-              columns: [
-                {
-                  Header: "Visits",
-                  accessor: "visits"
-                }
-              ]
+        <ReactTable data={data}
+          columns={[{
+      columns: [{
+        Header: 'First Name',
+        accessor: 'firstName'
+      }, {
+        Header: 'Last Name',
+        id: 'lastName',
+        accessor: d => d.lastName
+      }]
+    }, {
+      columns: [{
+        Header: 'Profile Progress',
+        accessor: 'progress',
+        Cell: row => (
+          <div
+            style={{
+              width: '100%',
+              height: '100%',
+              backgroundColor: '#dadada',
+              borderRadius: '2px'
+            }}
+          >
+            <div
+              style={{
+                width: `${row.value}%`,
+                height: '100%',
+                backgroundColor: row.value > 66 ? '#85cc00'
+                  : row.value > 33 ? '#ffbf00'
+                  : '#ff2e00',
+                borderRadius: '2px',
+                transition: 'all .2s ease-out'
+              }}
+            />
+          </div>
+        )
+      }, {
+        Header: 'Status',
+        accessor: 'status',
+        Cell: row => (
+          <span>
+            <span style={{
+              color: row.value === 'relationship' ? '#ff2e00'
+                : row.value === 'complicated' ? '#ffbf00'
+                : '#57d500',
+              transition: 'all .3s ease'
+            }}>
+              &#x25cf;
+            </span> {
+              row.value === 'relationship' ? 'In a relationship'
+              : row.value === 'complicated' ? `It's complicated`
+              : 'Single'
             }
-          ]}
-          getTdProps={(state, rowInfo, column, instance) => {
-            return {
-              onMouseEnter: e =>
-                console.log("Cell - onMouseEnter", {
-                  state,
-                  rowInfo,
-                  column,
-                  instance,
-                  event: e
-                })
-            };
-          }}
+          </span>
+        )
+      }]
+    }]}
           defaultPageSize={10}
           className="-striped -highlight"
         />
-        <br />
-        <Tips />       
       </div>
     );
   }
