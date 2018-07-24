12f8e8253a72d1f2140d5c17c94dc7dbb7c5e253
jackplugged <sidneisimmon@gmail.com>
2018-07-23 22:36:50 -0300

    Debounced

--- src/components/content/main-content-wrapper.jsx
+++ src/components/content/main-content-wrapper.jsx
@@ -1,11 +1,80 @@
-import React from "react";
+import React, { PureComponent } from "react";
 
-const MainContentWrapper = ({ children }) => (
-    <div className="content-wrapper">
-    <section className="content">
-       {children}
-     </section>
-  </div>
+// Setup a timer
+var timeout;
+
+// Listen for resize events
+window.addEventListener("resize", function(event) {
+    console.log("no debounce");
+
+    // If there's a timer, cancel it
+    if (timeout) {
+      window.cancelAnimationFrame(timeout);
+    }
+
+    // Setup the new requestAnimationFrame()
+    timeout = window.requestAnimationFrame(function() {
+      // Run our scroll functions
+      console.log("debounced");
+    });
+  },
+  false
 );
+
+
+class MainContentWrapper extends PureComponent {
+
+
+  active = () =>{
+    this.fix()
+
+    const body = document.querySelector('body')
+
+    /*
+    $('body').removeClass(ClassName.holdTransition);
+
+
+    if (this.options.resetHeight) {
+      $('body, html, ' + Selector.wrapper).css({
+        'height'    : 'auto',
+        'min-height': '100%'
+      });
+    }
+
+    if (!this.bindedResize) {
+      $(window).resize(function () {
+        this.fix();
+        
+
+        $(Selector.logo + ', ' + Selector.sidebar).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+          this.fix();         
+        }.bind(this));
+      }.bind(this));
+
+      this.bindedResize = true;
+    }
+
+    $(Selector.sidebarMenu).on('expanded.tree', function () {
+      this.fix();      
+    }.bind(this));
+
+    $(Selector.sidebarMenu).on('collapsed.tree', function () {
+      this.fix();   
+    }.bind(this));
+    */
+  }
+
+  fix  = () =>{
+
+  }
+
+  render() {
+    return (
+      <div className="content-wrapper">
+        <section className="content">{this.props.children}</section>
+      </div>
+    );
+  }
+}
 
 export default MainContentWrapper;
