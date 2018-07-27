31ab44c69ae9a81b663939dc60ed3391a9b0f39e
jackplugged <sidneisimmon@gmail.com>
2018-07-27 17:50:45 -0300

    Menu simplificação

--- src/containers/menu/index.js
+++ src/containers/menu/index.js
@@ -1,5 +1,7 @@
 import Tables from "containers/menu/tables";
+import Multlevel from "containers/menu/multlevel";
 
-const menu = [  Tables ];
+
+const menu = [ Tables, Multlevel ];
 
 export default menu;
